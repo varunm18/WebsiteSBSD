905a53ffcd291fa066e893814ab3eb92bed736c0
= <anirudhp196@gmail.com>
2024-05-02 14:06:18 -0400

    Connected backend to frontend

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
+    res.json({"users": ["userOne", "userTwo", "userThree", "userFour"]})
 })
 
 app.listen(8080, () => {console.log("Server started on port 8080")})
