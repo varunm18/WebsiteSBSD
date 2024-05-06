4c8f6951f32a7bc2fb99216dff7b5a6a7b8f574e
varunm18 <=>
2024-05-06 17:17:15 -0400

    added Google Oauth for sign in/sign out functionality Google One Tap verification

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,39 @@
 import React, {useEffect, useState} from 'react'
+import { jwtDecode } from "jwt-decode";
 
 function App() {
 
+  const [user, setUser] = useState({})
+
   const [backendData, setBackendData] = useState([{}])
 
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID token: "+response.credential)
+    var userObject = jwtDecode(response.credential)
+    console.log(userObject)
+    setUser(userObject)
+    document.getElementById("signInDiv").hidden = true
+  }
+
+  function handleSignOut() {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false
+  }
+
   useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "281737967662-u1vbtb5gfano89kkl8e1j6f310d6o508.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    })
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "outline", size: "large", }
+    )
+
+    google.accounts.id.prompt()
+
     fetch("/api").then(
       response => response.json()
     ).then(
@@ -13,6 +42,16 @@ function App() {
       }
     )
   }, [])
+
+  // useEffect(() => {
+  //   fetch("/api").then(
+  //     response => response.json()
+  //   ).then(
+  //     data => {
+  //       setBackendData(data)
+  //     }
+  //   )
+  // }, [])
 
   return (
     <div>
@@ -24,6 +63,17 @@ function App() {
           <p key = {i}>{user}</p>
         ))
       )}
+
+      <div id="signInDiv"></div>
+      { Object.keys(user).length != 0 &&
+        <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
+      }
+      {user &&
+        <div>
+          <img src={user.picture}></img>
+          <h3>{user.name}</h3>
+        </div>
+      }
       
     </div>
   )
