10189ef24e662f3d32507f6aa817ba165a5dd76a
varunm18 <=>
2024-05-20 13:30:26 -0400

    added inputs, buttons and connected sql database to backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import React, {useEffect, useState} from 'react'
 import { jwtDecode } from "jwt-decode";
 import './App.css';
-import {Navbar, Nav, Container} from "react-bootstrap"
+import {Navbar, Nav, Container, FormSelect} from "react-bootstrap"
 import {Helmet} from "react-helmet"
+import Dropdown from "react-bootstrap/Dropdown";
+import Button from 'react-bootstrap/Button';
 
 
 function App() {
 
   const [user, setUser] = useState({})
-  // module.exports = { user };
 
-  const [backendData, setBackendData] = useState([{}])
+  const [backendData, setBackendData] = useState([])
+
+  const [query, setQuery] = useState()
 
   function handleCallbackResponse(response) {
     console.log("Encoded JWT ID token: "+response.credential)
@@ -41,10 +44,11 @@ function App() {
 
     google.accounts.id.prompt()
 
-    fetch("/api").then(
+    fetch("/api?grade=11").then(
       response => response.json()
     ).then(
       data => {
+        console.log(typeof data)
         setBackendData(data)
       }
     )
@@ -52,7 +56,7 @@ function App() {
 
   return (
     <div>
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar id = "Navbar" bg="dark" data-bs-theme="dark">
         <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
           <ul class="navbar-nav mr-auto">
               <li class="nav-item active">
@@ -66,16 +70,16 @@ function App() {
         </div>
         <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
+                <li id = "google" class="nav-item">
                   <div id="signInDiv"></div>
                   <div id="signOutDiv" hidden>
                   { Object.keys(user).length != 0 &&
-                    <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
+                    <button id = "signOutButton" onClick={ (e) => handleSignOut(e)}>Sign Out</button>
                   }
                   {user &&
                     <div>
-                      <img src={user.picture}></img>
-                      <h3 color='white'>{user.name}</h3>
+                      <img id = "userPicture" src={user.picture}></img>
+                      <h3 id = "username">{user.name}</h3>
                     </div>
                   }
                   </div>
@@ -84,12 +88,104 @@ function App() {
         </div>
       </Navbar>
 
-      {(typeof backendData.users == 'undefined') ? (
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <div class = "container">
+        <div class = "row">
+          <div class = "col-lg-6">
+            <div class = "well">
+              <h2 id = "dataHeading">Find Students</h2>
+              <div class = "btn-group">
+                <div class = "select">
+                  <p>Grade Level</p>
+                  <select id = "gradeLevel" class="form-select" aria-label="Default select example">
+                    <option selected>None</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                  </select>
+                </div>
+
+                <div class = "select">
+                  <p class = "selectParagraph">Calculated Race</p>
+                  <select id = "CalculatedRace" class="form-select" aria-label="Default select example">
+                    <option selected>None</option>
+                    <option value="1">White</option>
+                    <option value="2">Asian</option>
+                    <option value="3">Multiple Races</option>
+                    <option value="4">Black</option>
+                    <option value="5">Hispanic</option>
+                  </select>
+                </div>
+
+                <div class = "select">
+                <p class = "selectParagraph">Participated In Sports?</p>
+                  <select id = "sportParticipation" class="form-select" aria-label="Default select example">
+                    <option selected>None</option>
+                    <option value="1">Yes</option>
+                    <option value="2">No</option>
+                  </select>
+                </div>
+
+                <div class = "select">
+                <p class = "selectParagraph">Participated In Clubs?</p>
+                  <select id = "clubParticipation" class="form-select" aria-label="Default select example">
+                    <option selected>None</option>
+                    <option value="1">Yes</option>
+                    <option value="2">No</option>
+                  </select>
+                </div>
+
+                <div class = "select">
+                <p class = "selectParagraph">Has Detentions?</p>
+                  <select id = "hasDetention" class="form-select" aria-label="Default select example">
+                    <option selected>None</option>
+                    <option value="1">Yes</option>
+                    <option value="2">No</option>
+                  </select>
+                </div>
+
+                <div class = "select">
+                <p class = "selectParagraph">Has Suspensions?</p>
+                  <select id = "hasSuspensions" class ="form-select" aria-label="Default select example">
+                    <option selected>None</option>
+                    <option value="1">Yes</option>
+                    <option value="2">No</option>
+                  </select>
+                </div>
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+        <Button onClick = "findStudents()"id = "submitButton" variant="outline-light">Submit</Button>
+        <script>
+          function findStudents(){
+            
+          }
+        </script>
+      </div>
+
+      {(typeof backendData == 'undefined') ? (
         <p>Loading ...</p>
       ): (
-        backendData.users.map((user,i) => (
-          <p key = {i} >{user}</p>
-        ))
+        backendData.map((student, i) => 
+        <p key = {i} >{student.FirstName} {student.LastName} Grade: {student.GradeLevel}</p>
+        )
       )}
     </div>
   )
--- server/server.js
+++ server/server.js
@@ -1,8 +1,37 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./AttendanceData.db', (err) => {
+    if(err){
+        console.log(err.message);
+    }
+    else{
+        console.log("Connected to database");
+    }
+});
+
+// app.use(express.json());
 
 app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree", "userFour"]})
+    var sql = 'SELECT * FROM DemoData';
+    console.log(sql);
+    if(req.query.grade){
+        sql+=' WHERE GradeLevel='+req.query.grade;
+    }
+    if(req.query.race){
+        sql+=' WHERE CalculatedRace='+req.query.race;
+    }
+    db.all(sql, (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).send('Internal server error');
+        } else {
+            console.log(typeof rows)
+            res.json(rows);
+        }
+    });
+    sql='';
 })
 
 app.listen(8080, () => {console.log("Server started on port 8080")})
