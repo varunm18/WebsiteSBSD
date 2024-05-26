8a60d51a6253faae3eba20066ad9f0969cf94e9f
= <anirudhp196@gmail.com>
2024-05-26 13:05:12 -0400

    Added stuff on findTrends page

--- client/src/pages/findTrends.js
+++ client/src/pages/findTrends.js
@@ -1,10 +1,39 @@
 import React from 'react'
 import App from '../App'
+import {Navbar, Nav, Container, FormSelect} from "react-bootstrap"
+import {Helmet} from "react-helmet"
+import Dropdown from "react-bootstrap/Dropdown";
+import Button from 'react-bootstrap/Button';
+import {Link} from 'react-router-dom';
+
 
 function findTrends(){
   return (
     <div>
-        <p>Hello</p>
+      <Navbar id = "Navbar" bg="dark" data-bs-theme="dark">
+        <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+          <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                  <Helmet>
+              <style>{'body { background-color: lightblue; }'}</style>
+              </Helmet>
+
+              <h1 id = "InitialHeading">SBHS Attendence Analytics</h1>
+              </li>
+          </ul>
+        </div>
+        <div>
+          <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <Button id = "returnToHome" variant="outline-light">
+                  <Link to = '/'>Return To Home</Link>
+                </Button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </Navbar>
     </div>
   )
 }
